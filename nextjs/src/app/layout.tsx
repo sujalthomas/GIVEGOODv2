7819ce89d69a4dee660fcdf6b2bfd203c285c6f1
Adam Raźniewski <adam@razniewski.eu>
2025-02-16 10:29:18 +0100

    Ocean Theme

--- nextjs/src/app/layout.tsx
+++ nextjs/src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   let theme = process.env.NEXT_PUBLIC_THEME
   if(!theme) {
-    theme = "theme-sass2"
+    theme = "theme-sass3"
   }
   const gaID = process.env.NEXT_PUBLIC_GOOGLE_TAG;
   return (
